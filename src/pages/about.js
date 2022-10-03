a55ad138d2679bdf9a4bd81fc9ead0900b8fcd5b
Tanja Schmidt <tagretha@gmail.com>
2022-10-03 10:40:32 +0200

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About This Site</h1>
+    </main>);
+}
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Hello Frontend Masters!</h1>
+    </main>);
+}
