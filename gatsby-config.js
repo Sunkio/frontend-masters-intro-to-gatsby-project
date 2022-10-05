4191345f7cd7552fd386e55f8c4ba11a656d8d1f
Tanja Schmidt <tagretha@gmail.com>
2022-10-05 11:19:32 +0200

    add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.example.com',
+    title: 'Frontend Masters Intro to Gatsby',
+    description: 'Frontend Masters Inro to Gatsby Course Projects',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
+  },
+};
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
 export default function AboutPage() {
   return (
     <main>
       <h1>About This Site</h1>
+      <Link to={"/"}>Home</Link>
     </main>);
 }
