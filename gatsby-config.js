a90eb10cd8d3ac9bd244913abfa553d2dc01af3f
Tanja Schmidt <tagretha@gmail.com>
2022-10-10 17:32:23 +0200

    fixes defaul Layout (gatsby-config) & minor typos/refacored file name

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,31 @@ module.exports = {
   siteMetadata: {
     siteUrl: 'https://www.example.com',
     title: 'Frontend Masters Intro to Gatsby',
-    description: 'Frontend Masters Inro to Gatsby Course Projects',
+    description: 'Frontend Masters Intro to Gatsby Course Projects',
     image:
       'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
   },
   plugins: [
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-plugin-layout',
+        options: {
+          component: require.resolve('./src/components/layout.js'),
+        },
+      },
   ],
 };
